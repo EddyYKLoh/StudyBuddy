d1167d9ace49afd3633a0a4524d22e292239a6de
Eddy Y.K. Loh <ykloh94@gmail.com>
2016-10-02 02:47:55 +0800

    SignUp UI

--- app/src/main/java/com/ykloh/studybuddy/LoginActivity.java
+++ app/src/main/java/com/ykloh/studybuddy/LoginActivity.java
@@ -1,26 +1,40 @@
 package com.ykloh.studybuddy;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        getSupportActionBar().hide();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.TRANSPARENT);
         }
+
+        final Button createAccountButton = (Button) findViewById(R.id.createAccountButton);
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,SignUpActivity.class));
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/ykloh/studybuddy/SignUpActivity.java
@@ -0,0 +1,23 @@
+package com.ykloh.studybuddy;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.signUpToolbar);
+        setSupportActionBar(myToolbar);
+
+
+    }
+}
